fb0bf783bbf2d5432951b0df7c63b9fa19b62c9d
Pascal <pascalbreu@bluewin.ch>
2014-06-13 09:30:06 +0200

    insert weather in rio de janeiro and manaus

--- js/scripts.js
+++ js/scripts.js
@@ -97,7 +97,86 @@ function success(pos) {
 
   });
 
+  // Rio de Janeiro auslesen
+  var address = 'Rua Professor Eurico Rabelo';
+
+  $.ajax({
+    url: 'http://maps.googleapis.com/maps/api/geocode/json',
+    data: {
+      address: address,
+      sensor: false
+    },
+    success: function(data) {
+      console.log(data);
+      
+      /**
+      * Anfrage an Forecast mit eingegebenen Koordinaten von Adresse
+      */
+      // Koordinaten von Anfrage
+      var lat = data.results[0].geometry.location.lat;
+      var lng = data.results[0].geometry.location.lng;
+      $('.js-address-janeiro').text(data.results[0].formatted_address);
+
+      $.ajax({ // Anfrage an Forecast mit lat und lang
+        url: 'https://api.forecast.io/forecast/08fe7a18b9273c9859e14b99ec1fe912/' + lat + ',' + lng,
+        data: {
+          units: 'si'
+        },
+        dataType: 'jsonp',
+        success: function(data) {
+          console.log(data);
+          $('.js-temp-janeiro').text(data.currently.temperature + '°C');
+          $('.js-wind-janeiro').text(data.currently.windSpeed + ' m pro Sekunde');
+        }
+      });
+    }
+  });
+
+
+  // Wetter in Manaus
+  $.ajax({
+      url: 'http://maps.googleapis.com/maps/api/geocode/json',
+      data: {
+        address: 'Manaus',
+        sensor: false
+      },
+      success: function(data) {
+        var lat = data.results[0].geometry.location.lat;
+        var lng = data.results[0].geometry.location.lng;
+        $('.js-address-manaus').text(data.results[0].formatted_address);
+
+        $.ajax({
+          url: 'https://api.forecast.io/forecast/a955df0e9afe8c822ebb3adf30265fb6/' + lat + ',' + lng,
+          data: {
+            units : 'si'
+          },
+          dataType: 'jsonp',
+          success: function(data) {
+            $('.js-weather-manaus').text(data.currently.summary + ' (' + data.currently.temperature + '°)');
+          }
+        });
+      }
+  });
+
+
+
+
 }
+
+var getWeahterData = function(lat, lng, callback) {
+  $.ajax({
+    url: 'https://api.forecast.io/forecast/a955df0e9afe8c822ebb3adf30265fb6/' + lat + ',' + lng,
+    data: {
+      units : 'si'
+    },
+    dataType: 'jsonp',
+    success: function(data) {
+      callback(data);
+    }
+  });
+};
+
+
 
 function error(err) {
   console.warn('ERROR(' + err.code + '): ' + err.message);
