7507317c033dd902a4b32b95c3e3035371790ac2
Pascal <pascalbreu@bluewin.ch>
2014-06-20 11:40:20 +0200

    local storage

--- js/service.js
+++ js/service.js
@@ -5,8 +5,34 @@ $(document).ready(function() {
 		maximumAge: 0
 	};
 
-	var success = function(pos) {
-		var crd = pos.coords;
+	// Default Sprache einstellen
+	if (localStorage.getItem('language') === null) {
+		localStorage.setItem('language', 'de');
+	}
+
+	// Default position setzen
+	if (localStorage.getItem('position') === null) {
+		localStorage.setItem('position', null);
+	}
+
+	var getAddress = function(pos) {
+		if (typeof pos !== 'undefined') {
+			localStorage.setItem('position', JSON.stringify(pos.coords));
+		}
+
+		var crd = JSON.parse(localStorage.getItem('position'));
+
+		$.ajax({
+			url: 'https://maps.googleapis.com/maps/api/geocode/json',
+			data: {
+				latlng: crd.latitude + ',' + crd.longitude,
+				sensor: true,
+				language: localStorage['language']
+			},
+			success: function(data) {
+				$('.js-current-address').text(data.results[0].formatted_address);
+			}
+		});
 
 		$('.js-current-position').text(crd.latitude + ', ' + crd.longitude);
 	};
@@ -15,6 +41,13 @@ $(document).ready(function() {
 		console.warn('ERROR(' + err.code + '): ' + err.message);
 	};
 
+	navigator.geolocation.getCurrentPosition(getAddress, error, options);
 
-	navigator.geolocation.getCurrentPosition(success, error, options);
+	$(document).on('change', '.js-language', function(e) {
+		localStorage['language'] = $(this).val();
+
+		getAddress();
+	});
+
+	$('.js-language').val(localStorage.getItem('language'));
 });
